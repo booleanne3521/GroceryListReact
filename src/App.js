50f15d65be7fb3340f2d7e14af723e4b9c3c4250
Annerlee <annerle.enderez@gmail.com>
2023-04-21 10:07:38 +0200

    Added styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Content from './Content';
 import Header from './Header';
 import Footer from './Footer';
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Header = () => {
+   
   return (
     <header>
        <h1>Groceries List</h1> 
