1d0993a1a1a5d803bd7a6a788099b458f8765760
Annerlee <annerle.enderez@gmail.com>
2023-04-21 10:26:21 +0200

    handle click events

--- src/Content.js
+++ src/Content.js
@@ -6,11 +6,25 @@ const Content = () => {
         const int = Math.floor(Math.random()*3);
         return names[int];
       }
+
+    const handleClick = () => {
+        console.log('You clicked it!')
+    }
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`)
+    }
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText)
+    }
+
   return (
     <main>
-        <p>
-Hello {handleNameChange()}!
+        <p onDoubleClick={handleClick}>
+            Hello {handleNameChange()}!
         </p>
+        <button onClick={handleClick}>Click it</button>
+        <button onClick={() =>{handleClick2('Jesus')}}>Click it</button>
+        <button onClick={(e) =>{handleClick3(e)}}>Click it</button>
     </main>
   )
 }
