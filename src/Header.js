a9da53474759445ad11b1272dfc293fe4127641f
Annerlee <annerle.enderez@gmail.com>
2023-04-21 14:27:53 +0200

    Props

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({title}) => {
    
   return (
     <header>
-       <h1>Groceries List</h1> 
+       <h1>{title}</h1> 
     </header>
   )
 }
-
+Header.defaultProps = {
+    title: "Inköpslista"
+}
 export default Header
