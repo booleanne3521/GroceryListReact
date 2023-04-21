eee9ef2d8990343e7c5184fa24ecd966d0cbd138
Annerlee <annerle.enderez@gmail.com>
2023-04-21 09:51:53 +0200

    created components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Content from './Content';
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-       Hello world!
-        </a>
-      </header>
+    <Header/>
+    <Content />
+    <Footer />
     </div>
   );
 }
